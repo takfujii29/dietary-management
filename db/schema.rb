20ea74c2ffd169e7bb4c9b5541a9fb9763f0c2cc
takfujii29 <takfujii29@icloud.com>
2019-12-24 17:14:13 +0900

    food-show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191223024607) do
+ActiveRecord::Schema.define(version: 20191224044602) do
 
   create_table "foods", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",         null: false
@@ -20,6 +20,26 @@ ActiveRecord::Schema.define(version: 20191223024607) do
     t.integer  "fat"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "management_foods", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "management_id"
+    t.integer  "food_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["food_id"], name: "index_management_foods_on_food_id", using: :btree
+    t.index ["management_id"], name: "index_management_foods_on_management_id", using: :btree
+  end
+
+  create_table "managements", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "food_id"
+    t.integer  "user_id"
+    t.string   "content"
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_managements_on_food_id", using: :btree
+    t.index ["user_id"], name: "index_managements_on_user_id", using: :btree
   end
 
   create_table "user_foods", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -45,6 +65,10 @@ ActiveRecord::Schema.define(version: 20191223024607) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "management_foods", "foods"
+  add_foreign_key "management_foods", "managements"
+  add_foreign_key "managements", "foods"
+  add_foreign_key "managements", "users"
   add_foreign_key "user_foods", "foods"
   add_foreign_key "user_foods", "users"
 end

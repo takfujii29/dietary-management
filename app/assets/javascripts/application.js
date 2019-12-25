adec98434ec38c5645a824fa4d98a26b520adfa1
takfujii29 <takfujii29@icloud.com>
2019-12-25 13:58:42 +0900

    calendar-display

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,32 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require moment
+//= require fullcalendar
 //= require_tree .
+
+$(function () {
+  // 画面遷移を検知
+  $(document).on('turbolinks:load', function () {
+    // lengthを呼び出すことで、#calendarが存在していた場合はtrueの処理がされ、無い場合はnillを返す
+    if ($('#calendar').length) {
+      function eventCalendar() {
+        return $('#calendar').fullCalendar({
+        });
+      };
+
+      function clearCalendar() {
+        $('#calendar').html('');
+      };
+
+      $(document).on('turbolinks:load', function () {
+        eventCalendar();
+      });
+      $(document).on('turbolinks:before-cache', clearCalendar);
+
+      $('#calendar').fullCalendar({
+        events: '/events.json'
+      });
+    }
+  });
+});

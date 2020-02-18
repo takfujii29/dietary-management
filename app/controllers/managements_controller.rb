c764906a147e6cb46b26a0ea548c06f82bf0957e
takfujii29 <takfujii29@icloud.com>
2020-02-18 17:34:13 +0900

    delete-today

--- app/controllers/managements_controller.rb
+++ app/controllers/managements_controller.rb
@@ -53,7 +53,7 @@ class ManagementsController < ApplicationController
     end
 
     def total
-      @today_managements = Management.where(day: Date.current).where(user_id: current_user.id)
+      @today_managements = Management.where(user_id: current_user.id)
       @total_calorie = 0
       @total_carbohydrate = 0
       @total_protein = 0
